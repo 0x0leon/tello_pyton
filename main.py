894ddd63e31994b0d99cc934f64da01070a243db
0x0leon <112933659+0x0leon@users.noreply.github.com>
2023-11-15 11:24:01 +0100

    Update: follow me code added

--- main.py
+++ main.py
@@ -3,18 +3,31 @@
 # > pip install --upgrade pip
 # > pip install -r requirements.txt
 
+import cv2
+import numpy as np
 from djitellopy import Tello
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
-drone = Tello()
 
 xList = []
 yList = []
 zList = []
+
+drone = Tello()
+drone.connect()
+drone.stream_on()
+
+cv2.namedWindow("Tello Television")
+
+follow_color_lower = np.array([30, 100, 100])
+follow_color_upper = np.array([60, 255, 255])
+
+# Set the desired distance for "follow me" (in pixels)
+follow_distance = 50
 
 '''
     graph operations
@@ -54,15 +67,41 @@ def plot_graph() -> None:
 def main():
     print("test")
 
-    running = True    
+
+    while True:
+
+        frame = drone.get_frame_read().frame
+
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        f_mask = cv2.inRange(hsv, follow_color_lower, follow_color_upper)
+        
+        cntrs, _ = cv2.findContours(f_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if cntrs:
+            largest_contoure = max(cntrs, key=cv2.contourArea)
+
+            Matrix  = cv2.moments(largest_contoure)
+            cx = int(Matrix["m10"] / Matrix["m00"])
+            cy = int(Matrix["m01"] / Matrix["m00"])
+
+            cv2.circle(frame, (cx, cy), 5, (0, 255, 0), -1)
+
+            drone_to_center = np.sqrt((cx - frame.shape[1] // 2)**2 + (cy - frame.shape[0] // 2)**2)
+
+            if drone_to_center > follow_distance:
+                drone.send_rc_control(0, 0, 30, 0)
+            else: 
+                drone.send_rc_control(0, 0, 0, 0)
+
+        cv2.imshow("Tello Television Stream", frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            break
+    drone.streamoff()
+    cv2.destroyAllWindows()
     
-    while running:
-        print("running")
 
-
-
-
-
-    
 if __name__ == "__main__":
     main()
